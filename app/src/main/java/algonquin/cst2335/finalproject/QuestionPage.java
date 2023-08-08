b08a2df6fffddec7ff12f9221743554913584752
mohds <sali0116@algonquinlive.com>
2023-08-07 23:52:05 -0400

    Trivia Question Database: adding interactability

--- app/src/main/java/algonquin/cst2335/finalproject/QuestionPage.java
+++ app/src/main/java/algonquin/cst2335/finalproject/QuestionPage.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
@@ -12,21 +14,30 @@ import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import algonquin.cst2335.finalproject.databinding.QuestionPageBinding;
+import algonquin.cst2335.finalproject.databinding.QuizSelectionPageBinding;
 
 public class QuestionPage extends AppCompatActivity {
+
+    @NonNull QuestionPageBinding binding;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_page);
+        binding = QuestionPageBinding.inflate(getLayoutInflater());
 
         Intent fromPrevious = getIntent();
         String questionNumber = fromPrevious.getStringExtra("questionNumber");
-
         SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("Email Address",questionNumber);
+        editor.putString("questionNumber", questionNumber);
         editor.apply();
 
         String url = null;
@@ -35,21 +46,41 @@ public class QuestionPage extends AppCompatActivity {
          * json object URL
          */
         try {
-            url = "https://opentdb.com/api.php?amount="
-                    + URLEncoder.encode(String.valueOf(questionNumber),"UTF-8")
-                    +"&category=22&type=multiple";
+            url = "https://opentdb.com/api.php?amount=" + URLEncoder.encode(questionNumber,"UTF-8")
+                + "&category=22&type=multiple";
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                (response) -> {
-                    try {
-                        JSONArray triviaQuiz = response.getJSONArray("api.php");
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
+            (response) -> {
+                try {
+                    JSONArray triviaQuiz = response.getJSONArray("results");
+                    JSONObject questionObject = triviaQuiz.getJSONObject(0);
+                    JSONObject mainObject = response.getJSONObject("main");
+                    String question = mainObject.getString("question");
+                    String correctAnswer = mainObject.getString("correct_answer");
+                    JSONArray incorrectAnswers = questionObject.getJSONArray("incorrect_answers");
+
+                    binding.questionText.setText(question);
+
+                    ArrayList<Button> answers = new ArrayList<>();
+                    answers.add(binding.answerA);
+                    answers.add(binding.answerB);
+                    answers.add(binding.answerC);
+                    answers.add(binding.answerD);
+
+                    Random rand = new Random();
+                    int answerRandomizer = rand.nextInt(4);
+                    answers.get(answerRandomizer).setText(correctAnswer);
+                    for (int i = 0 ; i > 2 ; i++){
+                        answers.get(answerRandomizer).setText(incorrectAnswers.getString(i));
                     }
-                },(error) -> {
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            },(error) -> {
 
         });
     }
--- app/src/main/java/algonquin/cst2335/finalproject/QuizSelectionPage.java
+++ app/src/main/java/algonquin/cst2335/finalproject/QuizSelectionPage.java
@@ -1,5 +1,7 @@
 package algonquin.cst2335.finalproject;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -20,26 +22,31 @@ public class QuizSelectionPage extends AppCompatActivity {
         setContentView(R.layout.quiz_selection_page);
         binding = QuizSelectionPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        int questionNumber = Integer.parseInt(binding.questionNumber.getText().toString());
         EditText questionNumberItem = binding.questionNumber;
 
         binding.startTest.setOnClickListener(click -> {
+            if(!binding.questionNumber.getText().toString().isEmpty()){
+
+                int questionNumber = Integer.parseInt(binding.questionNumber.getText().toString());
                 String url = null;
+
                 if(questionNumber > 50 || questionNumber == 0) {
                     Toast.makeText(this, "cannot select over 50 questions:", Toast.LENGTH_LONG).show();
                 }
                 else{
+                    SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("questionNumber", questionNumberItem.getText().toString());
+                    editor.apply();
+
                     Intent nextPage = new Intent( QuizSelectionPage.this, QuestionPage.class);
                     nextPage.putExtra("questionNumber",questionNumber);
+
                     startActivity(nextPage);
                     Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
-                    SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-                    questionNumberItem.setText(prefs.getString("questionNumber",""));
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putString("Email Address", questionNumberItem.getText().toString());
-                    editor.apply();
-//            binding..setOnClickListener(click -> {});
+                    //binding..setOnClickListener(click -> {});
                 }
-            });
+            }
+        });
     }
 }
